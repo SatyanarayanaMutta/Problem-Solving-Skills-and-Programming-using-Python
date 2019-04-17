d2a7e620ba8f2845fe5f891c69ad58b6eb4d9736
Naga Satyanarayana Mutta <47346796+SatyanarayanaMutta@users.noreply.github.com>
2019-04-17 18:33:01 +0530

    3rd

---
+++ distance converter.py
@@ -0,0 +1,12 @@
+cm = int(input("Enter Distance in cm--> "))
+  
+# Converting centimeter  
+# into meter and kilometer 
+meter = cm / 100.0; 
+kilometer = cm / 100000.0; 
+  
+#  Converted result
+print("Length in meter = " , 
+               meter , "m"); 
+print("Length in Kilometer = ",  
+             kilometer , "km"); 
---
+++ multiplication table.py
@@ -0,0 +1,3 @@
+table=int(input("Which table u want? "))
+for z in range(1,int(input("How many multiplications u want? "))+1):
+    print(table,'*',z,'=',table*z)
